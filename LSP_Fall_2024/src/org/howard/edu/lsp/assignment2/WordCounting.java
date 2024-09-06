e5affd72b1e8ab7e7ef96390d74d46300884fb5f
gloriadagrin <gloriadagrin@localhost>
2024-09-05 20:28:16 -0400

    third commit, adding comments

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -11,14 +11,19 @@ import java.util.Map;
 public class WordCounting {
 
 	public static void main(String[] args) {
-		Map<String, Integer> wordMap = new HashMap<String, Integer>();
+		//creating variables
+		Map<String, Integer> wordHash = new HashMap<String, Integer>();
 		BufferedReader reader = null;
 		
 		try {
+			//reading the words.txt file
 			reader = new BufferedReader(new FileReader("words.txt"));
+			//removing any special characters on the first line of the text file
 			String currentLine = reader.readLine().replaceAll("[^a-zA-Z]", " ");
 						
 			while(currentLine != null) {
+				//make all the words lower case
+				//Easier to compare words regardless if it's upper case or lower case
 				String words[] = currentLine.toLowerCase().split(" ");
 				//currentLine = reader.replaceAll("[^a-zA-Z]", "");
 				
@@ -26,14 +31,18 @@ public class WordCounting {
 				for(String word : words) {
 					//String word1 = word.replaceAll("[^a-zA-Z]", "");
 					if (!word.isBlank()) {
+						//each word is being looked at to see if there's any special characters
 						String word1 = word.replaceAll("[^a-zA-Z]", "");
-						if(wordMap.containsKey(word1)) {
-							wordMap.put(word1, wordMap.get(word1)+1);
+						if(wordHash.containsKey(word1)) {
+							//if the word exist in the wordHash, then the count goes up.
+							wordHash.put(word1, wordHash.get(word1)+1);
 						}else {
-							wordMap.put(word1, 1);
+							//if this is the first time the word is being up, just add 1 to the count
+							wordHash.put(word1, 1);
 						}
 					}
 				}
+				//again to the next line in the text file
 				currentLine = reader.readLine();
 			}
 			
@@ -49,7 +58,8 @@ public class WordCounting {
 				e.printStackTrace();
 			}
 		}
-		wordMap.forEach((k, v) -> System.out.println(k + "  " + v));
+		//printing out the word and the count
+		wordHash.forEach((k, v) -> System.out.println(k + "  " + v));
 
 	}
 
