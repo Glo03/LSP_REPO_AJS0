ed91c207df3bd9198935bf4f51c5c115cc2cfda0
gloriadagrin <gloriadagrin@localhost>
2024-10-14 20:00:47 -0400

    First commit for midterm.

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
 *  Name: Gloria Dagrin
 */
-package org.howard.edu.lsp.assignment2;
+package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
